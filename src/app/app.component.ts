2fd8607fafac77bf9e5ef6b3be21255db4a527e5
reez89 <nicola.rizzi89@hotmail.it>
2021-03-13 21:47:40 +0100

    chiamata async

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
 
-
+  courses$: Observable<Course[]>;
   courses;
 
   constructor(private http: HttpClient) {
@@ -27,10 +27,7 @@ export class AppComponent implements OnInit {
         .set('page','1')
         .set('pageSize','10');
 
-    this.http.get('/api/courses', {params})
-    .subscribe(
-      courses => this.courses = courses
-    );
+    this.courses$ = this.http.get<Course[]>('/api/courses', {params});
 
   }
 
